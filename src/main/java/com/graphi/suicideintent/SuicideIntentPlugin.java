8933ca95bdcefa0da6fafb8b960cdd34b4eaf811
denkers <kyle.business@inbox.com>
2016-02-27 22:17:51 +1300

    Changed artifactId/groupId in pom, initialized maven project, added suicideintent/SuicideIntentPlugin

---
+++ src/main/java/com/graphi/suicideintent/SuicideIntentPlugin.java
@@ -0,0 +1,21 @@
+//=========================================
+//  Kyle Russell
+//  AUT University 2015
+//  https://github.com/denkers/graphi-suic-plugin
+//=========================================
+
+package com.graphi.suicideintent;
+
+import com.graphi.app.AppManager;
+import com.graphi.plugins.AbstractPlugin;
+
+public class SuicideIntentPlugin extends AbstractPlugin
+{
+    public SuicideIntentPlugin()
+    {
+        super("SuicideIntent", "Suicide intent plugin");
+    }
+    
+    @Override
+    public void attachPanel(AppManager appManager) {}
+}
