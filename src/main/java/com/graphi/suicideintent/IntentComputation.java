c145ab2b4d0d6eec24e2fe09d0055fde48f96687
denkers <kyle.business@inbox.com>
2016-02-28 20:32:59 +1300

    Added suicideintent/IntentComputation

---
+++ src/main/java/com/graphi/suicideintent/IntentComputation.java
@@ -0,0 +1,12 @@
+///================================================
+//  Kyle Russell
+//  AUT University 2015
+//  https://github.com/denkers/graphi-suic-plugin
+//================================================
+
+package com.graphi.suicideintent;
+
+public class IntentComputation 
+{
+    
+}
