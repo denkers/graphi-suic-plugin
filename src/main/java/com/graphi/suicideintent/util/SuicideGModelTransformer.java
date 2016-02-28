5650bd726f1aca753dd5a0689e68279458766041
denkers <kyle.business@inbox.com>
2016-02-28 21:40:03 +1300

    Updated util/SuicideGModelTransformer with generation of suicide model matrix

--- src/main/java/com/graphi/suicideintent/util/SuicideGModelTransformer.java
+++ src/main/java/com/graphi/suicideintent/util/SuicideGModelTransformer.java
@@ -9,18 +9,22 @@ package com.graphi.suicideintent.util;
 import com.graphi.util.Edge;
 import com.graphi.util.Node;
 import edu.uci.ics.jung.graph.Graph;
+import edu.uci.ics.jung.graph.util.EdgeType;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import org.apache.commons.collections15.Transformer;
 
 public class SuicideGModelTransformer implements Transformer<Graph<Node, Edge>, double[][]>
 {
-    private double directedWeight;
-    private double undirectedWeight;
-    private double selfWeight;
-    private double perspectiveIndex;
+    private final double directedWeight;
+    private final double undirectedWeight;
+    private final double selfWeight;
+    private final int perspectiveIndex;
     
     public SuicideGModelTransformer(int perspectiveIndex)
     {
-        this(perspectiveIndex, 1.0, 0.5, 2);
+        this(perspectiveIndex, 1.0, 0.5, 2.0);
     }
     
     public SuicideGModelTransformer(int perspectiveIndex, double directedWeight, double undirectedWeight, double selfWeight)
@@ -36,6 +40,33 @@ public class SuicideGModelTransformer implements Transformer<Graph<Node, Edge>, 
     {
         int n               =   g.getVertexCount();
         double[][] matrix   =   new double[n][n];
+        
+        List<Node> vertices   =  new ArrayList<>(g.getVertices());
+        
+        for(int row = 0; row < n; row++)
+        {
+            Node current    =   vertices.get(row);
+            for(int col = 0; col < n; col++)
+            {
+                if(row == col && col == perspectiveIndex)
+                    matrix[perspectiveIndex][perspectiveIndex]  =   selfWeight;
+                
+                else
+                {
+                    Node next   =   vertices.get(col);
+                    if(g.isNeighbor(current, next))
+                    {
+                        Edge edge   =   g.findEdge(current, next);
+                        if(edge.getEdgeType() == EdgeType.UNDIRECTED)
+                            matrix[row][col] = undirectedWeight;
+                        else
+                            matrix[row][col] = directedWeight;
+                    }
+                    
+                    else matrix[row][col] = 0;
+                }
+            }
+        }
         
         return matrix;
     }
