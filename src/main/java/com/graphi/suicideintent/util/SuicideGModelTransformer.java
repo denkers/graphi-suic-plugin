7232bf15cbbd2c64311fac3053dbe7d0d1f655a0
denkers <kyle.business@inbox.com>
2016-02-28 21:16:15 +1300

    Updated util/SuicideGModelTransformer with constructors & transform skeleton

--- src/main/java/com/graphi/suicideintent/util/SuicideGModelTransformer.java
+++ src/main/java/com/graphi/suicideintent/util/SuicideGModelTransformer.java
@@ -6,7 +6,37 @@
 
 package com.graphi.suicideintent.util;
 
-public class SuicideGModelTransformer 
+import com.graphi.util.Edge;
+import com.graphi.util.Node;
+import edu.uci.ics.jung.graph.Graph;
+import org.apache.commons.collections15.Transformer;
+
+public class SuicideGModelTransformer implements Transformer<Graph<Node, Edge>, double[][]>
 {
+    private double directedWeight;
+    private double undirectedWeight;
+    private double selfWeight;
+    private double perspectiveIndex;
     
+    public SuicideGModelTransformer(int perspectiveIndex)
+    {
+        this(perspectiveIndex, 1.0, 0.5, 2);
+    }
+    
+    public SuicideGModelTransformer(int perspectiveIndex, double directedWeight, double undirectedWeight, double selfWeight)
+    {
+        this.perspectiveIndex   =   perspectiveIndex;
+        this.directedWeight     =   directedWeight;
+        this.undirectedWeight   =   undirectedWeight;
+        this.selfWeight         =   selfWeight;
+    }
+    
+    @Override
+    public double[][] transform(Graph<Node, Edge> g)
+    {
+        int n               =   g.getVertexCount();
+        double[][] matrix   =   new double[n][n];
+        
+        return matrix;
+    }
 }
