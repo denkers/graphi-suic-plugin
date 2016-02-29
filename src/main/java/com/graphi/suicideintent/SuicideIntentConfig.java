be04bc2e311c531e92cbf8a9becafadc45acf748
denkers <kyle.business@inbox.com>
2016-02-29 23:28:38 +1300

    Updated SuicideIntentConfig with factory getConfig for loading config and init SuicideIntentConfig

--- src/main/java/com/graphi/suicideintent/SuicideIntentConfig.java
+++ src/main/java/com/graphi/suicideintent/SuicideIntentConfig.java
@@ -6,6 +6,16 @@
 
 package com.graphi.suicideintent;
 
+import static com.graphi.app.ConfigManager.GLOBAL_CONF_FILE;
+import java.io.File;
+import java.io.IOException;
+import javax.swing.JOptionPane;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
 
 public class SuicideIntentConfig 
 {
@@ -26,6 +36,26 @@ public class SuicideIntentConfig
         this.selfWeight         =   selfWeight;
     }
     
+    public static SuicideIntentConfig getConfig()
+    {
+        try
+        {
+            File configFile             =   new File(GLOBAL_CONF_FILE);
+            DocumentBuilder docBuilder  =   DocumentBuilderFactory.newInstance().newDocumentBuilder();
+            Document configDoc          =   docBuilder.parse(configFile);
+            double undirectedW          =   Double.parseDouble(configDoc.getElementsByTagName("directedWeight").item(0).getTextContent());
+            double directedW            =   Double.parseDouble(configDoc.getElementsByTagName("undirectedWeight").item(0).getTextContent());
+            double selfW                =   Double.parseDouble(configDoc.getElementsByTagName("selfWeight").item(0).getTextContent());
+            
+            return new SuicideIntentConfig(directedW, undirectedW, selfW);
+        }
+        
+        catch(Exception e)
+        {
+            JOptionPane.showMessageDialog(null, "[Error] Failed to read " + CONFIG_FILE);
+            return new SuicideIntentConfig();
+        }
+    }
     
     public double getDirectedWeight() 
     {
