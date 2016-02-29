b35dcff29ddfb05143f80f41cf158ebb8a1202e6
denkers <kyle.business@inbox.com>
2016-02-29 23:12:35 +1300

    Updated SuicideIntentConfig with directedWeight, undirectedWeight and selfWeight fields and getters

--- src/main/java/com/graphi/suicideintent/SuicideIntentConfig.java
+++ src/main/java/com/graphi/suicideintent/SuicideIntentConfig.java
@@ -9,5 +9,36 @@ package com.graphi.suicideintent;
 
 public class SuicideIntentConfig 
 {
+    public static final String CONFIG_FILE  =   "SuicideIntentConfig.xml";
+    private double directedWeight;
+    private double undirectedWeight;
+    private double selfWeight;
+
+    public SuicideIntentConfig()
+    {
+        this(1.0, 0.5, 2.0);
+    }
     
+    public SuicideIntentConfig(double directedWeight, double undirectedWeight, double selfWeight)
+    {
+        this.directedWeight     =   directedWeight;
+        this.undirectedWeight   =   undirectedWeight;
+        this.selfWeight         =   selfWeight;
+    }
+    
+    
+    public double getDirectedWeight() 
+    {
+        return directedWeight;
+    }
+
+    public double getUndirectedWeight() 
+    {
+        return undirectedWeight;
+    }
+
+    public double getSelfWeight() 
+    {
+        return selfWeight;
+    }
 }
