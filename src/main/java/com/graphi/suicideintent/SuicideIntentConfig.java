687783e54a32d8fcf8bc9c38836a7f1d95b7e2be
denkers <kyle.business@inbox.com>
2016-02-29 23:04:11 +1300

    Added suicideintent/SuicideIntentConfig

---
+++ src/main/java/com/graphi/suicideintent/SuicideIntentConfig.java
@@ -0,0 +1,13 @@
+///================================================
+//  Kyle Russell
+//  AUT University 2015
+//  https://github.com/denkers/graphi-suic-plugin
+//================================================
+
+package com.graphi.suicideintent;
+
+
+public class SuicideIntentConfig 
+{
+    
+}
