eec9d7b64d31b5b673be131329318dd22220bf59
denkers <kyle.business@inbox.com>
2016-03-01 22:57:38 +1300

    Added perspective id and reset/compute controls in layout/SuicideIntentControlPanel

--- src/main/java/com/graphi/suicideintent/layout/SuicideIntentControlPanel.java
+++ src/main/java/com/graphi/suicideintent/layout/SuicideIntentControlPanel.java
@@ -9,16 +9,21 @@ package com.graphi.suicideintent.layout;
 import com.graphi.app.Consts;
 import java.awt.BorderLayout;
 import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JSpinner;
+import javax.swing.SpinnerNumberModel;
 import net.miginfocom.swing.MigLayout;
 
 public class SuicideIntentControlPanel extends JPanel
 {
     private JCheckBox displaySizeCheck, displayColourCheck;
     private JComboBox computeBox;
+    private JSpinner perspectiveSpinner;
+    private JButton resetButton, computeButton;
     
     public SuicideIntentControlPanel()
     {
@@ -28,16 +33,24 @@ public class SuicideIntentControlPanel extends JPanel
         displaySizeCheck    =   new JCheckBox("Display size");
         displayColourCheck  =   new JCheckBox("Display colour");
         computeBox          =   new JComboBox();
+        perspectiveSpinner  =   new JSpinner(new SpinnerNumberModel(0, 0, 10000, 1));
+        resetButton         =   new JButton("Reset");
+        computeButton       =   new JButton("Compute");
         
         computeBox.addItem("All");
         computeBox.addItem("Selected");
         
         JPanel panelWrapper =   new JPanel(new MigLayout("fillx"));
+        panelWrapper.setBackground(Consts.PRESET_COL);
+        
         panelWrapper.add(displaySizeCheck);
         panelWrapper.add(displayColourCheck, "wrap");
         panelWrapper.add(new JLabel("Target options"));
-        panelWrapper.add(computeBox);
-        panelWrapper.setBackground(Consts.PRESET_COL);
+        panelWrapper.add(computeBox, "wrap");
+        panelWrapper.add(new JLabel("Perspective ID"));
+        panelWrapper.add(perspectiveSpinner, "wrap");
+        panelWrapper.add(resetButton, "al right");
+        panelWrapper.add(computeButton);
         
         add(panelWrapper);
     }
