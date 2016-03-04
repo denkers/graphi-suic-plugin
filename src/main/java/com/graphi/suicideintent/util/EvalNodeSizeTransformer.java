865039ac1bdc28a377fd61ccb6df726a1b804636
denkers <kyle.business@inbox.com>
2016-03-04 23:42:37 +1300

    Added util/EvalNodeSizeTransformer

---
+++ src/main/java/com/graphi/suicideintent/util/EvalNodeSizeTransformer.java
@@ -0,0 +1,18 @@
+///================================================
+//  Kyle Russell
+//  AUT University 2015
+//  https://github.com/denkers/graphi-suic-plugin
+//================================================
+
+package com.graphi.suicideintent.util;
+
+import com.graphi.util.Node;
+import java.awt.Shape;
+import org.apache.commons.collections15.Transformer;
+
+
+public class EvalNodeSizeTransformer implements Transformer<Node, Shape>
+{
+    @Override
+    public Shape transform(Node i)  {}
+}
