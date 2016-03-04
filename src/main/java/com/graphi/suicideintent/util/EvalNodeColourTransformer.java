3138d022f20c283914e8e6a7517ebf7a30fda30e
denkers <kyle.business@inbox.com>
2016-03-04 23:43:51 +1300

    Added util/EvalNodeColourTransformer

---
+++ src/main/java/com/graphi/suicideintent/util/EvalNodeColourTransformer.java
@@ -0,0 +1,18 @@
+///================================================
+//  Kyle Russell
+//  AUT University 2015
+//  https://github.com/denkers/graphi-suic-plugin
+//================================================
+
+package com.graphi.suicideintent.util;
+
+import com.graphi.util.Node;
+import java.awt.Color;
+import org.apache.commons.collections15.Transformer;
+
+
+public class EvalNodeColourTransformer implements Transformer<Node, Color>
+{
+    @Override
+    public Color transform(Node i) {}
+}
