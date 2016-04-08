4fd1b2579ab5440b22910f113d5a100a15ea4836
denkers <kyle.business@inbox.com>
2016-04-08 23:48:49 +1200

    Added removal of edges and nodes simulation in sim/SuicideSimulation

--- src/main/java/com/graphi/suicideintent/sim/SuicideSimulation.java
+++ src/main/java/com/graphi/suicideintent/sim/SuicideSimulation.java
@@ -1,14 +1,34 @@
 package com.graphi.suicideintent.sim;
 
+import com.graphi.util.Edge;
+import com.graphi.util.Node;
+import edu.uci.ics.jung.graph.Graph;
+import java.util.Collection;
+import java.util.Random;
+
 public class SuicideSimulation
 {
-    public static void deleteNodes(double p)
+    public static void deleteNodes(double p, Graph<Node, Edge> graph)
     {
-        
+        Random rGen                 =   new Random();   
+        Collection<Node> nodes      =   graph.getVertices();
+        for(Node node : nodes)
+        {
+            double prob =   rGen.nextDouble();
+            if(prob <= p)
+                graph.removeVertex(node);
+        }
     }
     
-    public static void deleteEdges(double p)
+    public static void deleteEdges(double p, Graph<Node, Edge> graph)
     {
-        
+        Random rGen =   new Random();
+        Collection<Edge> edges      =   graph.getEdges();
+        for(Edge edge : edges)
+        {
+            double prob =   rGen.nextDouble();
+            if(prob <= p)
+                graph.removeEdge(edge);
+        }
     }
 }
