05168aa191c08e298bb8a82155132832469b8bd1
denkers <kyle.business@inbox.com>
2016-07-28 09:01:16 +1200

    Added tasks.ClearDeleteTask

---
+++ src/main/java/com/graphi/suicideintent/tasks/ClearDeleteTask.java
@@ -0,0 +1,29 @@
+///================================================
+//  Kyle Russell
+//  AUT University 2015
+//  https://github.com/denkers/graphi-suic-plugin
+//================================================
+
+package com.graphi.suicideintent.tasks;
+
+import com.graphi.suicideintent.layout.SuicideControlPanel;
+import com.graphi.tasks.AbstractTask;
+
+public class ClearDeleteTask extends AbstractTask
+{
+    @Override
+    public void initTaskDetails()
+    {
+        name    =   "Reset Suicide Simulation";
+    }
+
+    @Override
+    public void initDefaultProperties() {}
+
+    @Override
+    public void performTask() 
+    {
+        SuicideControlPanel.getSuicidePanelInstance()
+                .getSimPanel().clearDeadObjects();
+    }
+}
