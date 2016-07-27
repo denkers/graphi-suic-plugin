0eb88dc0276d9ca8ea347d0d21bdd74f279cb22c
denkers <kyle.business@inbox.com>
2016-07-28 09:03:53 +1200

    Added tasks.SuicIndexTask

---
+++ src/main/java/com/graphi/suicideintent/tasks/SuicIndexTask.java
@@ -0,0 +1,29 @@
+///================================================
+//  Kyle Russell
+//  AUT University 2015
+//  https://github.com/denkers/graphi-suic-plugin
+//================================================
+
+package com.graphi.suicideintent.tasks;
+
+import com.graphi.suicideintent.layout.SuicideControlPanel;
+import com.graphi.tasks.AbstractTask;
+
+public class SuicIndexTask extends AbstractTask
+{
+    @Override
+    public void initTaskDetails() 
+    {
+        name    =   "Suicide Index Computation";
+    }
+
+    @Override
+    public void initDefaultProperties() {}
+
+    @Override
+    public void performTask() 
+    {
+        SuicideControlPanel.getSuicidePanelInstance()
+                .getComputePanel().computeAverageSuicideIntent();
+    }
+}
